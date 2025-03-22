7e7fb0679b5d256680524d43727c36ff08f1ac87
Joel <joel020105@gmail.com>
2025-03-22 00:30:36 -0600

    importar y exportar usuarios excel

--- backend-py-main/routes/usuario.py
+++ backend-py-main/routes/usuario.py
@@ -8,6 +8,6 @@ usuario_routes.route("/", methods=["POST"])(agregar_usuario)
 usuario_routes.route("/", methods=["GET"])(obtener_usuarios)
 usuario_routes.route("/<id>", methods=["GET"])(obtener_usuario)
 usuario_routes.route("/<id>", methods=["PUT"])(actualizar_usuario)
-usuario_routes.route("/<id>", methods=["DELETE"])(eliminar_usuario)  # Ruta para eliminar un usuario
+usuario_routes.route("/<id>", methods=["DELETE"])(eliminar_usuario)  
 usuario_routes.route("/import-excel", methods=["POST"])(importar_excel)
 usuario_routes.route("/export-excel", methods=["GET"])(exportar_excel)
